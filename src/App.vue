b0a3d52becb98d82f71f3e1c06c9dcd229fdb8a7
kxzjoker <160348243+kxzjoker@users.noreply.github.com>
2024-12-29 13:34:19 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <Header title="骤雨重山图床" desc="拒绝流量劫持，全面使用HTTPS" />
-  <main><RouterView /></main>
+  <Header title="看戏仔图床" desc="拒绝流量劫持，全面使用HTTPS" />
+  <main><RouterView /></main>  
   <Footer />
   <Toaster />
 </template>
 <script setup lang="ts">
 import Header from '@/components/Header/Header.vue';
-import Footer from '@/components/Footer/Footer.vue';
+import Footer from '@/components/Footer/Footer.vue';  
 import { Toaster } from '@/components/ui/toast';
 </script>
 
